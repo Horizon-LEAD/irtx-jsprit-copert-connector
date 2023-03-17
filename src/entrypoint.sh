d8822aca52af27f7ebbaebdd8b773a24596fe913
Andreas Alexopoulos <andalexo@gmail.com>
2023-03-17 16:42:48 +0200

    fixes minor issues; fixes help msg

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-
 #Set fonts
 NORM=`tput sgr0`
 BOLD=`tput bold`
 REV=`tput smso`
+
+CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 function show_usage () {
     echo -e "${BOLD}Basic usage:${NORM} entrypoint.sh [-vh] FIN_PERSONS FIN_HOMES SEED SCALING OUT_PATH"
@@ -14,16 +15,16 @@ function show_help () {
     echo -e "${BOLD}eentrypoint.sh${NORM}: Runs the Parcels Model"\\n
     show_usage
     echo -e "\n${BOLD}Required arguments:${NORM}"
-    echo -e "${REV}CONF_FILE${NORM}\t the persons csv input file"
-    echo -e "${REV}SCENARIO${NORM}\t the homes gpkg input file"
-    echo -e "${REV}SOLUTION${NORM}\t the random seed"
-    echo -e "${REV}YEAR${NORM}\t the random seed"
-    echo -e "${REV}OUT_PATH${NORM}\t the scaling factor"
+    echo -e "${REV}CONF_FILE${NORM}\t the configuration json file"
+    echo -e "${REV}SCENARIO${NORM}\t the jsprit scenario file"
+    echo -e "${REV}SOLUTION${NORM}\t the jsprit solution file"
+    echo -e "${REV}YEAR${NORM}\t the year to be used"
+    echo -e "${REV}OUT_PATH${NORM}\t the output path"
     echo -e "${BOLD}Optional arguments:${NORM}"
     echo -e "${REV}-v${NORM}\tSets verbosity level"
     echo -e "${REV}-h${NORM}\tShows this message"
     echo -e "${BOLD}Examples:${NORM}"
-    echo -e "entrypoint.sh -v configuration.json scenario.json solution.json year ./output/"
+    echo -e "entrypoint.sh -v configuration.json scenario.json solution.json 2022 ./output/"
 }
 
 ##############################################################################
@@ -65,7 +66,7 @@ OUT_PATH=${leftovers[4]}
 ##############################################################################
 # Input checks                                                               #
 ##############################################################################
-if [ ! -d "${CONF_FILE}" ]; then
+if [ ! -f "${CONF_FILE}" ]; then
      echo -e "Give a ${BOLD}valid${NORM} path to the conversion configuration file\n"; show_usage; kill -INT $$
 fi
 if [ ! -f "${SCENARIO}" ]; then
@@ -74,9 +75,7 @@ fi
 if [ ! -f "${SOLUTION}" ]; then
     echo -e "Give a ${BOLD}valid${NORM} Path to the JSprit solution file\n"; show_usage; kill -INT $$
 fi
-if [ ! -f "${YEAR}" ]; then
-    echo -e "Give a ${BOLD}valid${NORM} Year\n"; show_usage; kill -INT $$
-fi
+
 if [ ! -f "${OUT_PATH}" ]; then
     echo -e "Give a ${BOLD}valid${NORM} output directory\n"; show_usage; kill -INT $$
 fi
@@ -84,7 +83,7 @@ fi
 ##############################################################################
 # Execution                                                                  #
 ##############################################################################
-papermill "/srv/app/src/Convert.ipynb" /dev/null \
+papermill ${CURDIR}/Convert.ipynb /dev/null \
   -pconfiguration_path ${CONF_FILE} \
   -pscenario_path ${SCENARIO} \
   -psolution_path ${SOLUTION} \
